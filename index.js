8a96c205549c2024347de383b0f9f87a78d4d190
koder-03 <163158547+koder-03@users.noreply.github.com>
2025-07-04 04:43:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-document.querySelector(".w").addEventListener("click", function(){
-    var audio= document.querySelector("#tom1");
-    audio.play();
-} )
-document.querySelector(".a").addEventListener("click", function(){
-    var audio2= document.querySelector("#tom2");
-    audio2.play();
-} )
-document.querySelector(".s").addEventListener("click", function(){
-    var audio3= document.querySelector("#tom3");
-    audio3.play();
-} )
-document.querySelector(".d").addEventListener("click", function(){
-    var audio= document.querySelector("#tom4");
-    audio.play();
-} )
-document.querySelector(".j").addEventListener("click", function(){
-    var audio= document.querySelector("#snare");
-    audio.play();
-} )
-document.querySelector(".k").addEventListener("click", function(){
-    var audio= document.querySelector("#crash");
-    audio.play();
-} )
-document.querySelector(".l").addEventListener("click", function(){
-    var audio= document.querySelector("#kick");
-    audio.play();
-} )
+var drums= document.querySelectorAll(".drum");
+for( let i=0; i<drums.length; i++ ){
+    drums[i].addEventListener("click",function(){
+        var audio= document.querySelectorAll("audio")[i];
+        audio.play();
+    })
+}
+document.addEventListener("keypress", function (event){
+    makenoise(event.key);
+})
+function makenoise( key){
+    for(let i=0; i<drums.length; i++){
+        if( drums[i].textContent==key){
+            var audio= document.querySelectorAll("audio")[i];
+            audio.play();
+        }
+    }
+}
